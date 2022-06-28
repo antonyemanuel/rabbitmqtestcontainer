13df2d22d0e1d89bdb966ea61fd962ed0fb76054
Antonio Cruz <antonio.cruz@ilia.digital>
2022-06-28 02:25:43 -0300

    add testcontainer with rabbitMQ

---
+++ src/test/java/io/gaddings/messaging/demomessaging/DemomessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.gaddings.messaging.demomessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
